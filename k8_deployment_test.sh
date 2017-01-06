3b174c84ba21c5150520ae4dce6d99073402a2bc
Vincent Schwarzer <vincent.schwarzer@yahoo.de>
2017-01-06 11:06:30 +0100

    Added function for retries

--- k8_deployment_test.sh
+++ k8_deployment_test.sh
@@ -7,7 +7,18 @@ set -ex
 
 #DEFINE APPLICATION variables
 RETRIES=5
-WAITSEC=30
+WAITSEC=5
+
+
+function delete {
+    echo "Destroy Service"
+    kubectl delete services nginx-service
+    command_success
+
+    echo "Destroy Deployment"
+    kubectl delete deployment nginx-deployment
+    command_success
+}
 
 function command_success {
     if [[ $? -ne 0 ]]; then
@@ -16,59 +27,55 @@ function command_success {
     fi
 }
 
+
+function checkIfAvailable {
+    n=0
+    while true; do
+        if [[ $n -lt $RETRIES ]]; then
+            avail_pods=$(kubectl get deployment nginx-deployment -o jsonpath='{.status.availableReplicas}')
+            if [[ $avail_pods -eq $1 ]]; then
+                break
+            fi
+            n=$[$n+1]
+            echo "Check failed. Retry $n of $RETRIES waiting $WAITSEC for retry."
+            sleep $WAITSEC
+        else
+            echo "Creation of Pods failed."
+            exit 1
+        fi
+    done
+}
+
+
+#Clean reamining previous deployments if error
+#echo "Cleanup old failed deployments"
+#delete
+
 #Setup Deployment
 echo "Creating Deployment"
 kubectl create -f ./nginx-deployment.yaml
 command_success
 
-echo "Waiting 15 seconds before progressing further"
-sleep 15
+echo "Waiting $WAITSEC seconds before progressing further"
+sleep $WAITSEC
+
 #Create Deployment of Appication
 req_pods=$(kubectl get deployment  nginx-deployment -o jsonpath='{.status.replicas}')
 
+checkIfAvailable $req_pods
 
-n=0
-while true; do
-    if [[ $n -lt 5 ]]; then
-        avail_pods=$(kubectl get deployment nginx-deployment -o jsonpath='{.status.availableReplicas}')
-        if [[ $avail_pods -eq $req_pods ]]; then
-            break
-        fi
-        n=$[$n+1]
-        echo "Waiting for $num_pods to be available. Retry $n of 5"
-        sleep 5
-    else
-        echo "Deployment of Pods failed"
-        exit 1
-    fi
-done
 
 
 #Scale Deployment (Current Pods * 2)
 
 req_pods=$[$req_pods*2]
 #echo "${req_pods}"
-
+echo "Scale deployment by doubling number of Replicas"
 kubectl scale deployment --replicas=$req_pods nginx-deployment
 command_success
 
-n=0
-while true; do
-
-    if [[ $n -lt 5 ]]; then
-        avail_pods=$(kubectl get deployment nginx-deployment -o jsonpath='{.status.availableReplicas}')
-        if [[ $avail_pods -eq $req_pods ]]; then
-            break
-        fi
-        n=$[$n+1]
-        echo "Waiting for $num_pods to be available. Retry $n of 5"
-        sleep 5
-    else
-        echo "Creation of Pods failed"
-        exit 1
-    fi
-done
-
+echo "Check if Pods are available."
+checkIfAvailable $req_pods
 
 
 #Test Service
@@ -81,10 +88,5 @@ kubectl get service nginx-service
 command_success
 
 #Destroy deployment
-echo "Destroy Service"
-kubectl delete services nginx-service
-command_success
-
-echo "Destroy Deployment"
-kubectl delete deployment nginx-deployment
-command_success
+echo "Cleanup current deployment / service"
+delete
